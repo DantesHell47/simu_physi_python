1feff82be6c0eecdfd117db31b22b6d7f2cd23fa
DantesHell47 <Jefferson.dante47@gmail.com>
2024-01-16 00:14:24 -0300

    Refatorei o código. Deixei as condições de colisões em funções

--- simu_phys_colision.py
+++ simu_phys_colision.py
@@ -6,10 +6,22 @@ plano = box(pos=vector(0, -0.5, 0), size=vector(10, 0.1, 10), color=color.white)
 bola1 = sphere(pos=vector(-1, 0, 0), radius=0.5, color=color.blue)
 bola2 = sphere(pos=vector(1, 0, 0), radius=0.5, color=color.green)
 
-
 # Velocidades iniciais das bolinhas
 bola1.velocity = vector(0.1, 0, 0)
 bola2.velocity = vector(-0.1, 0, 0)
+
+# Aqui é a função para verificar a colisao entre as bordas do plano e as bolinhas
+def colisao_bolas_planos(bola1, bola2, plano):
+    if (bola1.pos.x < -plano.size.x / 2 + bola1.radius) or (bola1.pos.x > - bola1.radius + plano.size.x / 2):
+        bola1.velocity *= -1 
+    if (bola2.pos.x < -plano.size.x / 2 + bola2.radius) or (bola2.pos.x > plano.size.x / 2 - bola2.radius):
+        bola2.velocity *= -1
+
+# Função para colisão entre as bolas
+def colisao_entre_bolas(bola1, bola2):
+    if mag(bola1.pos - bola2.pos) < 2 * bola1.radius:
+        bola1.velocity *= -1
+        bola2.velocity *= -1
 
 # Aqui no loop  while é que a magia acontece (Animação)
 while True:
@@ -20,14 +32,6 @@ while True:
     bola2.pos += bola2.velocity
 
     # Estas são as codições de colisão nas bodas do plano
-
-    if (bola1.pos.x < -plano.size.x / 2 + bola1.radius) or (bola1.pos.x > - bola1.radius + plano.size.x / 2):
-        bola1.velocity *= -1 
-    if (bola2.pos.x < -plano.size.x / 2 + bola2.radius) or (bola2.pos.x > plano.size.x / 2 - bola2.radius):
-        bola2.velocity *= -1 
-    
+    colisao_bolas_planos(bola1,bola2, plano)  
     # E estas aaqui são as coidções de colisões entre as bolinhas
-    
-    if mag(bola1.pos - bola2.pos) < 2 * bola1.radius:
-        bola1.velocity *= -1
-        bola2.velocity *= -1
+    colisao_entre_bolas(bola1, bola2)
